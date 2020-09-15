a00c1e84b320084e6f0a95ac34dedc028f0dde52
Caio Estevam <caioferreiramendes@gmail.com>
2020-09-15 11:30:27 -0300

    SELECT DAO

--- index.php
+++ index.php
@@ -2,16 +2,29 @@
 
 require_once("config.php");
 
-// $sql = new Sql();
 
-// $usuarios = $sql->select("SELECT * FROM tb_usuarios ");
 
-// echo json_encode($usuarios);
+// $root = new Usuario();
 
-$root = new Usuario();
+// $root -> loadById(7);
 
-$root -> loadById(7);
+// echo $root."</br>";
 
-echo $root;
+
+
+// $lista = Usuario::getList();
+
+// echo json_encode($lista);
+
+
+// $search = Usuario :: search("za");
+
+// echo json_encode($search);
+
+
+$usuario = new Usuario();
+$usuario->login("caio", "besta");
+
+echo $usuario;
 
 ?>
